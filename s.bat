1da4539d41fc92d4804a95b681fbaef7cbbf3edc
aandrei <aandrei@7ec92016-0320-0410-acc4-a06ded1c099a>
2001-10-20 01:04:37 +0000

    no message
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@5 7ec92016-0320-0410-acc4-a06ded1c099a

--- s.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;"C:\Program Files\GNU\WinCvs 1.2";"C:\Program Files\SSH Communications Security\SSH Secure Shell"
-set CVSROOT=:ext:aandrei@cvs.loki-lib.sourceforge.net:/cvsroot/loki-lib
-set CVS_RSH=ssh2
