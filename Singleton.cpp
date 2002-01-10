086288f0403e2c5bd2585f423451d262fe303a0d
aandrei <aandrei@7ec92016-0320-0410-acc4-a06ded1c099a>
2002-01-10 09:29:55 +0000

    // January 10, 2002: Fixed bug in call to realloc - credit due to Nigel Gent and //      Eike Petersen
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@10 7ec92016-0320-0410-acc4-a06ded1c099a

--- Singleton.cpp
+++ Singleton.cpp
@@ -8,7 +8,7 @@
 //     purpose is hereby granted without fee, provided that the above copyright 
 //     notice appear in all copies and that both that copyright notice and this 
 //     permission notice appear in supporting documentation.
-// The author or Addison-Welsey Longman make no representations about the 
+// The author or Addison-Wesley Longman make no representations about the 
 //     suitability of this software for any purpose. It is provided "as is" 
 //     without express or implied warranty.
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,7 +36,7 @@ void Loki::Private::AtExitFn()
     // Don't check errors - realloc with less memory 
     //     can't fail
     pTrackerArray = static_cast<TrackerArray>(std::realloc(
-        pTrackerArray, --elements));
+        pTrackerArray, sizeof(T) * --elements));
     // Destroy the element
     delete pTop;
 }
@@ -44,4 +44,6 @@ void Loki::Private::AtExitFn()
 ////////////////////////////////////////////////////////////////////////////////
 // Change log:
 // June 20, 2001: ported by Nick Thurn to gcc 2.95.3. Kudos, Nick!!!
+// January 10, 2002: Fixed bug in call to realloc - credit due to Nigel Gent and
+//      Eike Petersen
 ////////////////////////////////////////////////////////////////////////////////
