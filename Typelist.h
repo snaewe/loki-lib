4848129f403fa4eeb57be741ece61604010d5896
aandrei <aandrei@7ec92016-0320-0410-acc4-a06ded1c099a>
2002-05-13 20:01:46 +0000

    // May 13, 2002: TYPELIST_46 called TYPELIST_45 with only 44 parameters. //      Credit due to Robert Minsk
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@25 7ec92016-0320-0410-acc4-a06ded1c099a

--- Typelist.h
+++ Typelist.h
@@ -301,7 +301,7 @@
         T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, \
         T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, \
         T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, \
-        T41, T42, T43, T44, T45) >
+        T41, T42, T43, T44, T45, T46) >
 
 #define TYPELIST_47(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, \
         T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, \
@@ -746,6 +746,8 @@ namespace Loki
 //          Adam Wilkshire;
 //          Friedrik Hedman who fixed the bug but didn't send the fix;
 //          Kevin Cline who sent the first actual fix)
+// May 13, 2002: TYPELIST_46 called TYPELIST_45 with only 44 parameters.
+//      Credit due to Robert Minsk     
 ////////////////////////////////////////////////////////////////////////////////
 
 #endif // TYPELIST_INC_
