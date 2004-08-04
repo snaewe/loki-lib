7d7c562d6c289497dba9c8192c7d95a9dceb8edb
rani_sharoni <rani_sharoni@7ec92016-0320-0410-acc4-a06ded1c099a>
2004-08-04 14:56:18 +0000

    fix bug 960061

--- SmallObj.cpp
+++ SmallObj.cpp
@@ -6,19 +6,21 @@
 //////////////////////////////////
 
 #ifdef LOKI_USE_REFERENCE
-#	include "Reference/SmallObj.cpp"
+#   include "Reference/SmallObj.cpp"
 #else
-#	if (__INTEL_COMPILER)
-#		include "Reference/SmallObj.cpp"
-#	elif (__MWERKS__)
-#		include "Reference/SmallObj.cpp"
-#	elif (__BORLANDC__ >= 0x560)
-#		include "Borland/SmallObj.cpp"
-#	elif (_MSC_VER >= 1300)
-#		include "MSVC/1300/SmallObj.cpp"
-#	elif (_MSC_VER >= 1200)
-#		include "MSVC/1200/SmallObj.cpp"
-#	else
-#		include "Reference/SmallObj.cpp"
-#	endif
+#   if (__INTEL_COMPILER)
+#       include "Reference/SmallObj.cpp"
+#   elif (__MWERKS__)
+#       include "Reference/SmallObj.cpp"
+#   elif (__BORLANDC__ >= 0x560)
+#       include "Borland/SmallObj.cpp"
+#   elif (_MSC_VER >= 1301)
+#       include "Reference/SmallObj.cpp"
+#   elif (_MSC_VER >= 1300)
+#       include "MSVC/1300/SmallObj.cpp"
+#   elif (_MSC_VER >= 1200)
+#       include "MSVC/1200/SmallObj.cpp"
+#   else
+#       include "Reference/SmallObj.cpp"
+#   endif
 #endif
