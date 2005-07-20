13d5aec6526b3b2cdea3049839acd136323e853b
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-07-20 10:47:50 +0000

    moved source files
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@163 7ec92016-0320-0410-acc4-a06ded1c099a

--- include/noncc/loki/Singleton.cpp
+++ include/noncc/loki/Singleton.cpp
@@ -6,21 +6,21 @@
 ///////////////////////////////////
 
 #ifdef LOKI_USE_REFERENCE
-#   include "../../loki/Singleton.cpp"
+#   include "../../../src/Singleton.cpp"
 #else
 #   if (__INTEL_COMPILER)
-#       include "../../loki/Singleton.cpp"
+#       include "../../../src/Singleton.cpp"
 #   elif (__MWERKS__)
-#       include "../../loki/Singleton.cpp"
+#       include "../../../src/Singleton.cpp"
 #   elif (__BORLANDC__ >= 0x560)
 #       include "../Borland/Singleton.cpp"
 #   elif (_MSC_VER >= 1301)
-#       include "../../loki/Singleton.cpp"
+#       include "../../../src/Singleton.cpp"
 #   elif (_MSC_VER >= 1300)
 #       include "../MSVC/1300/Singleton.cpp"
 #   elif (_MSC_VER >= 1200)
 #       include "../MSVC/1200/Singleton.cpp"
 #   else
-#       include "../../loki/Singleton.cpp"
+#       include "../../../src/Singleton.cpp"
 #   endif
 #endif
