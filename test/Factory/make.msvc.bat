8a5cb93b2539320e86a76895680fb75027f6cd84
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-09-24 16:50:33 +0000

    update include path

--- test/Factory/make.msvc.bat
+++ test/Factory/make.msvc.bat
@@ -1,5 +1,5 @@
 if not exist tmp\ mkdir tmp
 
-cl -c -Zm200 -O2 -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\include" -I"..\include\loki" -Fotmp\ Factory.cpp
+cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ Factory.cpp
 
-link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" tmp\Factory.obj ..\lib\SmallObj.obj ..\lib\Singleton.obj 
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" tmp\Factory.obj ..\..\lib\SmallObj.obj ..\..\lib\Singleton.obj 
--- test/RegressionTest/make.msvc.bat
+++ test/RegressionTest/make.msvc.bat
@@ -1,5 +1,5 @@
 if not exist tmp\ mkdir tmp
 
-cl -c -Zm200 -O2 -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -I"..\..\include\loki" -Fotmp\ Test.cpp
+cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ Test.cpp
 
 link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" tmp\Test.obj 
