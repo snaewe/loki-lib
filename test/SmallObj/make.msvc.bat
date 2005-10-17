bf155755b5366c4783f1c069a5589fc92794151b
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-10-17 09:03:54 +0000

    add SmallSingleton to build process

--- test/SmallObj/make.msvc.bat
+++ test/SmallObj/make.msvc.bat
@@ -20,9 +20,19 @@ if "%1"=="dll" (
 
 @ECHO ON
 
+:: main.cpp
+
 cl -c -DNDEBUG -Zm200 -O2 -%MTMD% -EHsc -GR -W4 -wd4710 -I"." -I"..\..\include" -I"..\..\include\loki" -Fotmp\ SmallObjBench.cpp ..\..\src\SmallObj.cpp
 
 link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:%OUT_EXE% tmp\SmallObjBench.obj tmp\SmallObj.obj 
+
+
+:: SmallSingleton.cpp
+
+cl -c -DNDEBUG -Zm200 -O2 -%MTMD% -EHsc -GR -W4 -wd4710 -I"." -I"..\..\include" -I"..\..\include\loki" -Fotmp\ SmallSingleton.cpp ..\..\src\SmallObj.cpp ..\..\src\Singleton.cpp
+
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:SmallSingleton-msvc.exe tmp\SmallSingleton.obj tmp\SmallObj.obj tmp\Singleton.obj
+
 
 
 @ECHO OFF
