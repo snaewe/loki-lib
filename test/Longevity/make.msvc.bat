d75be3bd8c819b219ba2effdf923e8f9ca4a6786
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-10-17 08:24:50 +0000

    add example from Curtis Krauskopf's CUJ article 'Creating Dynamic Singletons & the Loki Library' - www.decompile.com
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@306 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/Longevity/make.msvc.bat
+++ test/Longevity/make.msvc.bat
@@ -1,5 +1,8 @@
 if not exist tmp\ mkdir tmp
 
+
+::  main.cpp
+
 cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ main.cpp
 
 if not defined LOKI_MSVC_NOLIB (
@@ -9,5 +12,20 @@ link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" ..\..\lib\l
 ) else (
 
 link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" tmp\main.obj ..\..\lib\SmallObj.obj ..\..\lib\Singleton.obj 
+
+)
+
+
+::  main2.cpp
+
+cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ main2.cpp
+
+if not defined LOKI_MSVC_NOLIB (
+
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main2-msvc.exe" ..\..\lib\loki.lib tmp\main2.obj 
+
+) else (
+
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main2-msvc.exe" tmp\main2.obj ..\..\lib\SmallObj.obj ..\..\lib\Singleton.obj 
 
 )
