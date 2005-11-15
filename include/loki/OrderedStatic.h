f32f61e5a9fbcbf7e14f025e3195383bf865b924
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-11-15 15:12:21 +0000

    don't use #include 'loki/..' for include/loki files
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@363 7ec92016-0320-0410-acc4-a06ded1c099a

--- include/loki/OrderedStatic.h
+++ include/loki/OrderedStatic.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // The Loki Library
-// Copyright (c) 2005 Peter Kümmel
+// Copyright (c) 2005 Peter Kmmel
 // Permission to use, copy, modify, distribute and sell this software for any 
 //     purpose is hereby granted without fee, provided that the above copyright 
 //     notice appear in all copies and that both that copyright notice and this 
@@ -20,9 +20,9 @@
 #include <vector>
 #include <iostream>
 
-#include "loki/Singleton.h"
-#include "loki/Typelist.h"
-#include "loki/Sequence.h"
+#include "Singleton.h"
+#include "Typelist.h"
+#include "Sequence.h"
 
 namespace Loki
 {
