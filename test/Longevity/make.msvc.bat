af80063cd32af11a10589a014c10379106ec0c17
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-12-19 07:54:40 +0000

    generate library also with the ms toolkit
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@383 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/Longevity/make.msvc.bat
+++ test/Longevity/make.msvc.bat
@@ -5,27 +5,13 @@ if not exist tmp\ mkdir tmp
 
 cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ main.cpp
 
-if not defined LOKI_MSVC_NOLIB (
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" ..\..\lib\loki.lib tmp\main.obj
 
-link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" ..\..\lib\loki.lib tmp\main.obj 
-
-) else (
-
-link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main-msvc.exe" tmp\main.obj ..\..\lib\SmallObj.obj ..\..\lib\Singleton.obj 
-
-)
 
 
 ::  main2.cpp
 
 cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" -Fotmp\ main2.cpp
 
-if not defined LOKI_MSVC_NOLIB (
+link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main2-msvc.exe" ..\..\lib\loki.lib tmp\main2.obj
 
-link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main2-msvc.exe" ..\..\lib\loki.lib tmp\main2.obj 
-
-) else (
-
-link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"main2-msvc.exe" tmp\main2.obj ..\..\lib\SmallObj.obj ..\..\lib\Singleton.obj 
-
-)
