d06ab97f12a57faceebeb3c4910ac5bb76f4265b
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2006-01-10 23:11:49 +0000

    make SafeFormat linkable
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@465 7ec92016-0320-0410-acc4-a06ded1c099a

--- src/make.msvc.bat
+++ src/make.msvc.bat
@@ -2,7 +2,7 @@ if not exist ..\lib\ mkdir ..\lib
 
 if not exist tmp\ mkdir tmp
 
-cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W4 -I"." -I"..\include" -Fo.\tmp\ SmallObj.cpp Singleton.cpp OrderedStatic.cpp
+cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W4 -I"." -I"..\include" -Fo.\tmp\ SmallObj.cpp Singleton.cpp OrderedStatic.cpp SafeFormat.cpp
 
-link /lib /NOLOGO /OUT:"..\lib\loki.lib" tmp\SmallObj.obj tmp\Singleton.obj tmp\OrderedStatic.obj
+link /lib /NOLOGO /OUT:"..\lib\loki.lib" tmp\SmallObj.obj tmp\Singleton.obj tmp\OrderedStatic.obj tmp\SafeFormat.obj
 
