dd6db69b426ae2ac02166ca15cda5451a1269652
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2006-03-08 16:50:18 +0000

    add register test code

---
+++ test/Register/classlist.h
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// The Loki Library
+// Copyright (c) 2006 Peter Kümmel
+// Permission to use, copy, modify, distribute and sell this software for any 
+//     purpose is hereby granted without fee, provided that the above copyright 
+//     notice appear in all copies and that both that copyright notice and this 
+//     permission notice appear in supporting documentation.
+// The author makes no representations about the 
+//     suitability of this software for any purpose. It is provided "as is" 
+//     without express or implied warranty.
+////////////////////////////////////////////////////////////////////////////////
+#ifndef CLASSLIST_H
+#define CLASSLIST_H
+
+// $Header$
+
+#include <string>
+
+#include <loki/Register.h>
+
+
+struct Base
+{
+	virtual void foo() = 0;
+};
+
+bool registerClass(std::string, Base*(*)() );
+
+typedef Loki::Seq
+<
+	struct Foo,
+	struct Boo
+
+>::Type ClassList;
+
+#endif
---
+++ test/Register/foo.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+// The Loki Library
+// Copyright (c) 2006 Peter Kümmel
+// Permission to use, copy, modify, distribute and sell this software for any 
+//     purpose is hereby granted without fee, provided that the above copyright 
+//     notice appear in all copies and that both that copyright notice and this 
+//     permission notice appear in supporting documentation.
+// The author makes no representations about the 
+//     suitability of this software for any purpose. It is provided "as is" 
+//     without express or implied warranty.
+////////////////////////////////////////////////////////////////////////////////
+#ifndef LOKI_FOO_H
+#define LOKI_FOO_H
+
+// $Header$
+
+#include "classlist.h"
+
+
+struct Foo : Base
+{
+	Foo();
+	void foo();
+};
+
+struct Boo : Base
+{
+	Boo();
+	void foo();
+};
+
+
+
+#endif
