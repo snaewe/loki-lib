a8726a5b4f21c4476592f278279c3efde1d6fb7f
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2006-05-20 11:46:17 +0000

    enable all warnings for msvc: -W4

--- test/DeletableSingleton/make.msvc.bat
+++ test/DeletableSingleton/make.msvc.bat
@@ -1,7 +1,7 @@
 
 ::  DeletableSingleton.cpp
 
-cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W0 -wd4710 -I"." -I"..\..\include" DeletableSingleton.cpp
+cl -c -Zm200 -O2 -DNDEBUG -MT -EHsc -GR -W4 -wd4710 -I"." -I"..\..\include" DeletableSingleton.cpp
 
 link /NOLOGO /SUBSYSTEM:CONSOLE /incremental:no /OUT:"DeletableSingleton-msvc.exe" ..\..\lib\loki.lib DeletableSingleton.obj 
 
