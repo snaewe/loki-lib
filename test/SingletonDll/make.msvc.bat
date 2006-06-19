085a019dae9b3ce918a80bbbce3eef9ad597299f
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2006-06-19 12:35:09 +0000

    fix msvc build process
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@679 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/SingletonDll/make.msvc.bat
+++ test/SingletonDll/make.msvc.bat
@@ -14,7 +14,7 @@ link /NOLOGO /DLL /OUT:"SingletonDll.dll" SingletonDll.obj foo.lib
 :: build Client.exe
 
 cl -c -DSINGLETONDLL_DLL -DFOO_DLL -Zm200 -O2 -DNDEBUG -MT  -EHsc -GR -W4 -wd4251 -I"." -I"..\..\include" client.cpp
-link /NOLOGO /SUBSYSTEM:CONSOLE /OUT:"Client.exe" Client.obj foo.lib SingletonDll.lib
+link /NOLOGO /SUBSYSTEM:CONSOLE /OUT:"Client-msvc.exe" Client.obj foo.lib SingletonDll.lib
 
 
 
