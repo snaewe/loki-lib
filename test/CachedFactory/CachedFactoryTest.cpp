d9dbd55441a92e1d2c1f031272a1914116d624c1
syntheticpp <syntheticpp@7ec92016-0320-0410-acc4-a06ded1c099a>
2006-10-17 21:10:41 +0000

    add copyright notice
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@763 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/CachedFactory/CachedFactoryTest.cpp
+++ test/CachedFactory/CachedFactoryTest.cpp
@@ -1,4 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// The Loki Library
+// Copyright (c) 2006 by Guillaume Chatelet
+//
+// Code covered by the MIT License
+//
+// Permission to use, copy, modify, distribute and sell this software for any 
+// purpose is hereby granted without fee, provided that the above copyright 
+// notice appear in all copies and that both that copyright notice and this 
+// permission notice appear in supporting documentation.
+//
+// The authors make no representations about the suitability of this software
+// for any purpose. It is provided "as is" without express or implied warranty.
+//
+// This code DOES NOT accompany the book:
+// Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
+//     Patterns Applied". Copyright (c) 2001. Addison-Wesley.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 // $Id$
+
 
 #define USE_SEQUENCE
 
