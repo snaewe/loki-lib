410fddbb13ca6465df9d61937ff07f934ea76221
Richard Sposato <rich_sposato@users.sourceforge.net>
2011-09-23 00:46:54 +0000

    Added text of MIT License.
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@1116 7ec92016-0320-0410-acc4-a06ded1c099a

--- src/Singleton.cpp
+++ src/Singleton.cpp
@@ -2,15 +2,27 @@
 // The Loki Library
 // Copyright (c) 2001 by Andrei Alexandrescu
 // This code accompanies the book:
-// Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
+// Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design
 //     Patterns Applied". Copyright (c) 2001. Addison-Wesley.
-// Permission to use, copy, modify, distribute and sell this software for any 
-//     purpose is hereby granted without fee, provided that the above copyright 
-//     notice appear in all copies and that both that copyright notice and this 
-//     permission notice appear in supporting documentation.
-// The author or Addison-Wesley Longman make no representations about the 
-//     suitability of this software for any purpose. It is provided "as is" 
-//     without express or implied warranty.
+// Code covered by the MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
 // $Id$
@@ -36,16 +48,16 @@ unsigned int Loki::Private::elements = 0;
 void LOKI_C_CALLING_CONVENTION_QUALIFIER Loki::Private::AtExitFn()
 {
     assert(pTrackerArray!=0 && !pTrackerArray->empty());
-    
+
     // Pick the element at the top of the stack
     LifetimeTracker* pTop = pTrackerArray->back();
-    
+
     // Remove that object off the stack _before_ deleting pTop
     pTrackerArray->pop_back();
-    
+
     // Destroy the element
     delete pTop;
-    
+
     // Destroy stack when it's empty _after_ deleting pTop
     if(pTrackerArray->empty())
     {
@@ -62,7 +74,7 @@ void LOKI_C_CALLING_CONVENTION_QUALIFIER Loki::Private::AtExitFn()
     // Pick the element at the top of the stack
     LifetimeTracker* pTop = pTrackerArray[elements - 1];
     // Remove that object off the stack
-    // Don't check errors - realloc with less memory 
+    // Don't check errors - realloc with less memory
     //     can't fail
     pTrackerArray = static_cast<TrackerArray>(std::realloc(
         pTrackerArray, sizeof(*pTrackerArray) * --elements));
@@ -70,5 +82,5 @@ void LOKI_C_CALLING_CONVENTION_QUALIFIER Loki::Private::AtExitFn()
     delete pTop;
 }
 
-#endif 
+#endif
 
