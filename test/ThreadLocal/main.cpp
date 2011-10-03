c3e6ca51b9da90dafd50d98d68539425e2bacaa2
Richard Sposato <rich_sposato@users.sourceforge.net>
2011-10-03 05:05:41 +0000

    Minor cosmetic changes. Also replaced system call with use of std::cin.get.
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@1134 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/ThreadLocal/main.cpp
+++ test/ThreadLocal/main.cpp
@@ -72,7 +72,9 @@ int main( int argc, const char * const argv[] )
     else
         cout << "Your compiler does not properly implement thread_local storage for standalone static values." << endl;
 
-    ::system( "pause" );
+    ::std::cout << "Please press enter key to continue." << ::std::endl;
+    ::std::cin.get();
+
     return 0;
 }
 
