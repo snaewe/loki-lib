d2c063039f762ca47cf7213408fb561d9437caed
Richard Sposato <rich_sposato@users.sourceforge.net>
2011-10-22 00:24:54 +0000

    Changed include paths.
    git-svn-id: https://loki-lib.svn.sourceforge.net/svnroot/loki-lib/trunk@1165 7ec92016-0320-0410-acc4-a06ded1c099a

--- test/LevelMutex/Thing.cpp
+++ test/LevelMutex/Thing.cpp
@@ -28,8 +28,8 @@
 #include <algorithm>
 #include <functional>
 
-#include <SafeFormat.h>
-#include <LockingPtr.h>
+#include <loki/SafeFormat.h>
+#include <loki/LockingPtr.h>
 
 using namespace ::Loki;
 
